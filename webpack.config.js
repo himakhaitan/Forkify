4967f4d4dc01fd6db18a66728ad3ec48358187fb
hima_khaitan <himanshukhaitan108@gmail.com>
2021-03-22 14:49:41 +0530

    Auto Reload on Dev mode!

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 
-const path = require('path')
+const path = require('path');
+const HtmlWebpackplugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js',
-    }
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js',
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new HtmlWebpackplugin({
+            filename: 'index.html',
+            template: './src/index.html',
+            
+        })
+    ]
 };
