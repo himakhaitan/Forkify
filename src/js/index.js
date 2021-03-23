331d61de040205daca54b3a49577c023a13582cc
hima_khaitan <himanshukhaitan108@gmail.com>
2021-03-24 03:00:38 +0530

    Pagination Added

--- src/js/index.js
+++ src/js/index.js
@@ -12,28 +12,43 @@ import {elements, renderLoader, clearLoader, elementStrings} from './views/base'
 const state = {};
 
 const controlSearch = async () => {
-  // Get the query
+  // 1) Get query from view
   const query = searchView.getInput();
-  console.log(query)
 
   if (query) {
-    // New Search Object and add it to the state
-    state.search = new Search(query);
+      // 2) New search object and add to state
+      state.search = new Search(query);
 
-    // Prepare UI for results
-     renderLoader(elements.searchRes)
-    // Search for Recipe
+      // 3) Prepare UI for results
+      searchView.clearInput();
+      searchView.clearResults();
+      renderLoader(elements.searchRes);
 
-    await state.search.getResults();
-
-    // render results on UI
-    clearLoader()
-    searchView.clearResults();
-    searchView.renderResults(state.search.result);
-    searchView.clearInput();
+      try {
+          // 4) Search for recipes
+          await state.search.getResults();
+  
+          // 5) Render results on UI
+          clearLoader();
+          searchView.renderResults(state.search.result);
+      } catch (err) {
+          alert('Something wrong with the search...');
+          clearLoader();
+      }
   }
-};
+}
+
 elements.searchForm.addEventListener('submit', e => {
-    e.preventDefault();
-    controlSearch();
+  e.preventDefault();
+  controlSearch();
+});
+
+
+elements.searchResPages.addEventListener('click', e => {
+  const btn = e.target.closest('.btn-inline');
+  if (btn) {
+      const goToPage = parseInt(btn.dataset.goto, 10);
+      searchView.clearResults();
+      searchView.renderResults(state.search.result, goToPage);
+  }
 });
