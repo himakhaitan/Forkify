a2f379095ce9061e31e502472b904ca794d9628e
hima_khaitan <himanshukhaitan108@gmail.com>
2021-03-23 12:06:42 +0530

    Recepies Rendered in UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,39 @@
 // Global app controller
 
-import num from './test';
-const x = 23;
-console.log(num + `${x} is yup!`);
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import {elements} from './views/base';
+
+// GLOBAL STATE
+// -- CURRENT RECEPIE
+// -- SEARCH OBJECT
+// -- SHOPPING LIST OBJECT
+// -- Liked Recepie
+const state = {};
+
+const controlSearch = async () => {
+  // Get the query
+  const query = searchView.getInput();
+  console.log(query)
+
+  if (query) {
+    // New Search Object and add it to the state
+    state.search = new Search(query);
+
+    // Prepare UI for results
+
+    // Search for Recipe
+
+    await state.search.getResults();
+
+    // render results on UI
+
+    searchView.clearResults();
+    searchView.renderResults(state.search.result);
+    searchView.clearInput();
+  }
+};
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
