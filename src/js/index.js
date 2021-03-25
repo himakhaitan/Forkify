2de3297b4876a32de4e0dd33f92c4b9aa8f02e36
hima_khaitan <himanshukhaitan108@gmail.com>
2021-03-26 00:41:21 +0530

    List Feature Added!

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import Recipe from "./models/Recipe";
 import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
+import * as listView from "./views/listView";
 import {elements, renderLoader, clearLoader, elementStrings} from './views/base';
 
 // GLOBAL STATE
@@ -103,6 +104,43 @@ const controlRecipe = async () => {
 ['hashchange', 'reload'].forEach(event => window.addEventListener(event, controlRecipe));
 
 
+
+/** 
+ * LIST CONTROLLER
+ */
+ const controlList = () => {
+  // Create a new list IF there in none yet
+  if (!state.list) state.list = new List();
+
+  // Add each ingredient to the list and UI
+  state.recipe.ingredients.forEach(el => {
+      const item = state.list.addItem(el.count, el.unit, el.ingredient);
+      listView.renderItem(item);
+  });
+}
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+
+  // Handle the delete button
+  if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+      // Delete from state
+      state.list.deleteItem(id);
+
+      // Delete from UI
+      listView.deleteItem(id);
+
+  // Handle the count update
+  } else if (e.target.matches('.shopping__count-value')) {
+      const val = parseFloat(e.target.value, 10);
+      if (val >= 0) {
+        state.list.updateCount(id, val);
+      }
+      
+  }
+});
+
 // Handlin Recipe Buttons
 
 elements.recipe.addEventListener('click', e=> {
@@ -116,6 +154,8 @@ elements.recipe.addEventListener('click', e=> {
   else if(e.target.matches('.btn-increase, .btn-increase *')){
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe)
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    controlList();
   }
 });
 
