305c14d0d854157fb3d5db668ed71c110a8d2416
hima_khaitan <himanshukhaitan108@gmail.com>
2021-03-25 23:42:23 +0530

    List Model Created

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
+import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
 import {elements, renderLoader, clearLoader, elementStrings} from './views/base';
@@ -116,4 +117,6 @@ elements.recipe.addEventListener('click', e=> {
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe)
   }
-})
+});
+
+
